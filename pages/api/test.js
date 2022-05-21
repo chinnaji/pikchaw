26a334460b1cc573a56f65b33ffc854d4c7c1bed
Chibuike Nnaji <chibuikennaji306@gmail.com>
2022-05-21 02:33:54 -0800

    removed api folder

--- pages/api/test.js
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import { searchUnsplash } from "../../helpers/searchImages";
-export default async function handler(req, res) {
-  const response = await searchUnsplash("1", "game");
-  res.status(200).json({ response });
-}
